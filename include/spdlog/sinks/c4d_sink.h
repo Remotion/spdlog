8cd8808b457c0d911682fb385b5e04b5cd6ab592
Remotion <remotion4d@gmail.com>
2016-10-20 16:55:06 +0200

    Optimized _sink_it() Added Hack to remove time part like this one [15:49:49.369]

--- include/spdlog/sinks/c4d_sink.h
+++ include/spdlog/sinks/c4d_sink.h
@@ -29,7 +29,16 @@ public:
 
 protected:
     void _sink_it(const details::log_msg& msg) override {
-        GePrint(String(msg.formatted.c_str(), STRINGENCODING_UTF8));
+        auto size = msg.formatted.size(); if (size == 0) { return; }
+        auto data = msg.formatted.data();
+        ///Hack to remove time part like this one [15:49:49.369] 
+        if (size > 16) {
+            data = data + 15; //TODO: is there a better way for this ?
+            size = size - 15;
+        }
+        String str;
+        str.SetCString(data, size, STRINGENCODING_UTF8);
+        GePrint(str);
     }
 };
 
